5f70379984ec3ed837d50858414fe388576cffe3
awsmdavid <awsmdavid@gmail.com>
2017-03-19 22:50:09 -0400

    adding data and parser

---
+++ parse_reviews.py
@@ -0,0 +1,20 @@
+import gzip
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def main():
+	for review in parse("reviews_Cell_Phones_and_Accessories_5.json.gz"):
+		# text to parse
+		write_to_file(review['reviewText'])
+
+def write_to_file(text):
+	# print(text)
+	with open("input.txt", "a") as output_file:
+		output_file.write(text)
+		output_file.write("\n")
+
+if __name__ =="__main__":
+    main()
