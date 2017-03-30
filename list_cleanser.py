ce57613d5b83fb17b8f814298b7d0ebcb44dafcc
davcheng <ddc88@cornell.edu>
2017-03-30 12:30:14 -0400

    adding al

---
+++ list_cleanser.py
@@ -0,0 +1,9 @@
+def main():
+	input = open("generated_list.txt", "r")
+	for line in input:
+	    if not line.startswith('I') and not line.startswith('Total') and not line.startswith('Free') and not line.startswith('pci') and not line.startswith('major') and not line.startswith('name'):
+	        with open("cleansed_generated_list.txt", "a") as output_file:
+			output_file.write(line)
+
+if __name__ == "__main__":
+	main()
