b444f8035e478415de34518492c1ffcfeec5207c
davcheng <ddc88@cornell.edu>
2017-03-30 12:37:27 -0400

    updated list

--- list_cleanser.py
+++ list_cleanser.py
@@ -1,5 +1,5 @@
 def main():
-	input = open("generated_list.txt", "r")
+	input = open("generated_list_032917.txt", "r")
 	for line in input:
 	    if not line.startswith('I') and not line.startswith('Total') and not line.startswith('Free') and not line.startswith('pci') and not line.startswith('major') and not line.startswith('name'):
 	        with open("cleansed_generated_list.txt", "a") as output_file:
