50a19f871634f94f3e66649eeea4dcff3d6ee091
davcheng <ddc88@cornell.edu>
2017-03-29 20:21:40 -0400

    adding new list

---
+++ text_generator.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def main():
+	generated_result = subprocess.Popen(['python', 'sample.py'], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+	out = generated_result.communicate()[0]
+	write_to_file(out)
+
+def write_to_file(text):
+	with open("generated_list.txt", "a") as output_file:
+		output_file.write(text)
+		# output_file.write("\n")
+
+if __name__ == "__main__":
+	for x in xrange(500): 
+		main()
